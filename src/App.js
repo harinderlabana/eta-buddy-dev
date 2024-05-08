ea18f68df3d5d5a0a89b306c6de76bfcaa712f37
harinderlabana <sunny.labana@gmail.com>
2024-05-08 15:20:35 -0400

    updated data to May 8,2024

--- src/App.js
+++ src/App.js
@@ -135,13 +135,13 @@ function App() {
         <>
           <h1 className="app-heading">etaBuddy</h1>
           <div className="last-modified">
-            Updated on May 7th 2024. Powered by Sorbet Solutions. 🍧
+            Updated on May 8th 2024. Powered by Sorbet Solutions. 🍧
           </div>
           <DataTable data={data} />
           <br />
           <h1 className="app-heading">orderBuddy</h1>
           <div className="last-modified">
-            Updated on May 7th 2024. Powered by Sorbet Solutions. 🍧
+            Updated on May 8th 2024. Powered by Sorbet Solutions. 🍧
           </div>
           <DataTable2 userSalespersonID={loggedInUserID} />
           <br />
