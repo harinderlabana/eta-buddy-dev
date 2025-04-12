d2c0afd1edce00ac837fa41aa648d9e74430a188
harinderlabana <sunny.labana@gmail.com>
2025-04-12 16:12:17 -0400

    program ended

--- src/Login.js
+++ src/Login.js
@@ -12,16 +12,7 @@ function Login({ onLogin }) {
 
   return (
     <div className="login-container">
-      <h3>
-        Thank you all for using this app. etaBuddy has been a passion project of
-        mine and I have learned alot about the customer and user experience
-        while building it. I am looking forward to moving my resources to other
-        projects and interests. If this is a tool you appreciated and valued the
-        use of contact your leaders and inquire about subscription services.
-        This will aid in supporting me to keep the app alive and updated as
-        development time, servers and costs are valuable and limited. Thank you,
-        Sunny Labana.
-      </h3>
+      <h3>404</h3>
     </div>
   );
 }
