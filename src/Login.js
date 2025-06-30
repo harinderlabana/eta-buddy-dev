b2bb81eca656d199844c6f4daac1e8de67e8ef6f
harinderlabana <sunny.labana@gmail.com>
2025-06-29 20:37:35 -0400

    brought site back online for demos

--- src/Login.js
+++ src/Login.js
@@ -1,18 +1,44 @@
-import React, { useState } from "react";
-import "./Login.css"; // Import the custom CSS file
+import React, { useState } from 'react';
+import './Login.css'; // Import the custom CSS file
 
 function Login({ onLogin }) {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
 
-  const handleSubmit = (e) => {
+  const handleSubmit = e => {
     e.preventDefault();
     onLogin(username, password);
   };
 
   return (
     <div className="login-container">
-      <h3>404</h3>
+        <h1 className="app-heading">etaBuddy</h1>
+        <div className="powered-by">Powered by Sorbet Solutions. 🍧</div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="username">Username: </label>
+          <input
+            type="email"
+            id="username"
+            value={username}
+            onChange={e => setUsername(e.target.value)}
+            required
+          />
+        </div>
+        <br></br>
+        <div>
+          <label htmlFor="password">Password: </label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+            required
+          />
+        </div>
+        <br></br>
+        <button type="submit">Login</button>
+      </form>
     </div>
   );
 }
